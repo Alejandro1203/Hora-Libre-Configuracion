533a2049966cf1de5677d40d8856c2efa14c8612
Alejandro1203 <alefg2003@gmail.com>
2025-01-08 21:04:08 +0100

    Cookie
    Ejercicios de las cookies terminado

--- JavaScript/EJERCICIOS/cookies/main.js
+++ JavaScript/EJERCICIOS/cookies/main.js
@@ -2,30 +2,37 @@ const aceptar = document.querySelector("#boton")
 const textArea = document.querySelector("#textArea")
 let intervalo;
 
-aceptar.addEventListener("click", e => {
-    let textValue = document.querySelector("#nombre").value;
-    docCookies.setItem("nombre", encodeURIComponent(textValue), 10);
+window.onload = () =>{
+    setInterval(updateCookieTextArea, 2000);
 
-    console.log(docCookies.getItem("nombre"));
-
-    document.querySelector("#divNombre").innerHTML = `<label>Hola ${textValue}!</label>`;
-
-    intervalo = setInterval(resetCookie, 10000);
-})
-
-function resetCookie() {
-    if(confirm("¿Sigues ahí?")) {
-        docCookies.setItem("nombre", docCookies.getItem("nombre"), 10);
-    } else {
-        document.querySelector("div").innerHTML = `<input id="nombre" type="text" placeholder="Nombre"/><button id="boton">Aceptar</button>`;
-        clearInterval(intervalo);
+    if(!docCookies.getItem("nombre")) {
+        alert("Bienvenido")
     }
-}
-
-function updateTextArea() {
-    docCookies.setItem("textArea", encodeURIComponent(document.querySelector("#nombre").value), 60);
 
     textArea.value = docCookies.getItem("textArea");
 }
 
-setInterval(updateTextArea, 2000);
+aceptar.addEventListener("click", e => {
+    e.preventDefault();
+
+    let textValue = document.querySelector("#nombre").value;
+    docCookies.setItem("nombre", encodeURIComponent(textValue), 1000);
+
+    document.querySelector("#divNombre").innerHTML = `<label>Hola ${textValue}!</label>`;
+
+    intervalo = setInterval(resetCookie, 5000);
+})
+
+function resetCookie() {
+    if(confirm("¿Sigues ahí?")) {
+        docCookies.setItem("nombre", docCookies.getItem("nombre"), 1000);
+    } else {
+        document.querySelector("div").innerHTML = `<input id="nombre" type="text" placeholder="Nombre"/><button id="boton">Aceptar</button>`;
+        docCookies.removeItem("nombre");
+        clearInterval(intervalo);
+    }
+}
+
+function updateCookieTextArea() {
+    docCookies.setItem("textArea", textArea.value, 5);
+}
