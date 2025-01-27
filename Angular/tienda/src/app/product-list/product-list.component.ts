1dcc5ef6b7fb3d16c44cf4a78bd2f3f28b98ccbc
Alejandro1203 <alefg2003@gmail.com>
2025-01-27 21:50:23 +0100

    Proyecto Angular Terminado

--- Angular/tienda/src/app/product-list/product-list.component.ts
+++ Angular/tienda/src/app/product-list/product-list.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { NgFor, NgIf } from '@angular/common';
 import { products } from '../products';
+import { ProductAlertsComponent } from '../product-alerts/product-alerts.component';
 
 @Component({
   selector: 'app-product-list',
-  imports: [NgFor, NgIf],
+  imports: [NgFor, NgIf, ProductAlertsComponent],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
   products = [...products]
+
+  share() {
+    window.alert("The product has been shared!")
+  }
+
+  onNotify() {
+    window.alert("You will be notified when the product goes on sale")
+  }
 }
+
